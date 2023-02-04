3959132c6315656c237e0f06bb9217e22de2477a
JohnKlenov <klenovptz@mail.ru>
2023-02-04 17:34:04 +0300

    Create NewHomeViewController

---
+++ FirstObserver/Controllers/NewHomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewHomeViewController.swift
+//  FirstObserver
+//
+//  Created by Evgenyi on 4.02.23.
+//
+
+import UIKit
+
+class NewHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
