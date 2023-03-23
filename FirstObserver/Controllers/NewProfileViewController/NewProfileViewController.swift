4a6ad733a3e86d325459dd6ff3a9fb56a900e62e
JohnKlenov <klenovptz@mail.ru>
2023-03-23 13:55:18 +0300

    NewProfileViewController -> Create UI

--- FirstObserver/Controllers/NewProfileViewController/NewProfileViewController.swift
+++ FirstObserver/Controllers/NewProfileViewController/NewProfileViewController.swift
@@ -10,19 +10,120 @@ import UIKit
 
 final class NewProfileViewController: UIViewController {
     
-    private let topConteinerView: UIView = {
+    private let topView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .darkGray
+        view.backgroundColor = .black
         return view
     }()
     
-    private let testImageView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .darkGray
-        view.layer.cornerRadius = 10
-        return view
+    private let imageUser: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+//        view.backgroundColor = .darkGray
+        image.layer.cornerRadius = 10
+        image.clipsToBounds = true
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    let userNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .none
+        textField.textAlignment = .center
+        textField.font = .systemFont(ofSize: 20, weight: .regular)
+        textField.tintColor = .black
+        textField.textContentType = .name
+        textField.backgroundColor = .clear
+        textField.placeholder = "enter you name"
+        textField.text = "Johan Klenov"
+        return textField
+    }()
+    
+    let emailUserTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .none
+        textField.textAlignment = .center
+        textField.font = .systemFont(ofSize: 17, weight: .medium)
+        textField.tintColor = .black
+        textField.textContentType = .emailAddress
+        textField.backgroundColor = .clear
+        textField.text = "klenovptz@mail.ru"
+        return textField
+    }()
+    
+    let infoUserStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.spacing = 8
+        stackView.backgroundColor = .lightGray
+        return stackView
+    }()
+   
+    let signInSignUp: UIButton = {
+    
+        var configuration = UIButton.Configuration.gray()
+
+        var container = AttributeContainer()
+        container.font = UIFont.boldSystemFont(ofSize: 17)
+        container.foregroundColor = UIColor.white
+        configuration.attributedTitle = AttributedString("SignIn/SignUp", attributes: container)
+
+        configuration.titleAlignment = .center
+        configuration.buttonSize = .large
+        configuration.baseBackgroundColor = .black
+        var grayButton = UIButton(configuration: configuration)
+        grayButton.translatesAutoresizingMaskIntoConstraints = false
+        return grayButton
+    }()
+    
+    let signOutButton: UIButton = {
+        
+        var configuration = UIButton.Configuration.gray()
+       
+        var container = AttributeContainer()
+        container.font = UIFont.boldSystemFont(ofSize: 17)
+        container.foregroundColor = UIColor.white
+        configuration.attributedTitle = AttributedString("Sign Out", attributes: container)
+       
+        configuration.titleAlignment = .center
+        configuration.buttonSize = .large
+        configuration.baseBackgroundColor = .black
+        var grayButton = UIButton(configuration: configuration)
+        grayButton.translatesAutoresizingMaskIntoConstraints = false
+        return grayButton
+    }()
+    
+    let deleteAccountButton: UIButton = {
+        
+        var configuration = UIButton.Configuration.gray()
+       
+        var container = AttributeContainer()
+        container.font = UIFont.boldSystemFont(ofSize: 17)
+        container.foregroundColor = UIColor.red
+        configuration.attributedTitle = AttributedString("Delete Account", attributes: container)
+       
+        configuration.titleAlignment = .center
+        configuration.buttonSize = .large
+        configuration.baseBackgroundColor = .black
+        var grayButton = UIButton(configuration: configuration)
+        grayButton.translatesAutoresizingMaskIntoConstraints = false
+        return grayButton
+    }()
+    
+    let buttonsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 8
+        return stackView
     }()
     
     let navBarRightButton: UIButton = {
@@ -43,7 +144,7 @@ final class NewProfileViewController: UIViewController {
     
     let navBarLeftButton: UIButton = {
         var configButton = UIButton.Configuration.plain()
-        configButton.title = "Edit"
+        configButton.title = "Cancel"
         configButton.baseForegroundColor = .systemPurple
         configButton.titleAlignment = .trailing
         configButton.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incomig in
@@ -57,6 +158,8 @@ final class NewProfileViewController: UIViewController {
         return button
     }()
     
+//    var cnstr: NSLayoutConstraint!
+    
 
     
     override func viewDidLoad() {
@@ -65,43 +168,115 @@ final class NewProfileViewController: UIViewController {
         
         view.backgroundColor = .white
         configureNavigationBar(largeTitleColor: .white, backgoundColor: .black, tintColor: .white, title: "Profile", preferredLargeTitle: false)
-        setupNavigationBar()
-    
+        configureNavigationItem()
+        setupStackView()
+        addActions()
         
+        view.addSubview(topView)
+        view.addSubview(imageUser)
+        view.addSubview(infoUserStackView)
+        view.addSubview(buttonsStackView)
         
-        //        topConteinerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
-        //        topConteinerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
-        //        topConteinerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
-        //        topConteinerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3).isActive = true
+        imageUser.image = UIImage(named: "DefaultImage")
+        setupConstraints()
+        
         
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-//        testImageView.frame  = CGRect(x: 0, y: 0, width: view.frame.height * 0.2, height: view.frame.height * 0.2)
-//        testImageView.center = CGPoint(x: topConteinerView.center.x, y: topConteinerView.frame.height)
+        
+//        imageUser.frame  = CGRect(x: 0, y: 0, width: view.frame.height * 0.2, height: view.frame.height * 0.2)
+//        imageUser.center = CGPoint(x: topView.center.x, y: topView.frame.height)
     }
     
     
     
     @objc func navBarRightButtonHandler() {
-        navBarLeftButton.configuration?.showsActivityIndicator = true
+        navBarRightButton.configuration?.showsActivityIndicator.toggle()
         print("navBarRightButtonHandler()")
     }
     
     @objc func navBarLeftButtonHandler() {
-        navBarLeftButton.configuration?.showsActivityIndicator = false
         print("navBarLeftButtonHandler()")
     }
     
     
-    func setupNavigationBar() {
+    func configureNavigationItem() {
         
         navBarRightButton.addTarget(self, action: #selector(navBarRightButtonHandler), for: .touchUpInside)
         navBarLeftButton.addTarget(self, action: #selector(navBarLeftButtonHandler), for: .touchUpInside)
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: navBarLeftButton)
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: navBarRightButton)
-
+    }
+    
+    func setupStackView() {
+        
+        infoUserStackView.addArrangedSubview(userNameTextField)
+        infoUserStackView.addArrangedSubview(emailUserTextField)
+        
+        buttonsStackView.addArrangedSubview(signInSignUp)
+        buttonsStackView.addArrangedSubview(signOutButton)
+        buttonsStackView.addArrangedSubview(deleteAccountButton)
+        
+    }
+    
+    func setupConstraints() {
+        topView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 0).isActive = true
+        topView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        topView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        topView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
+        
+        imageUser.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
+        imageUser.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
+        imageUser.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        let yConstraint = NSLayoutConstraint(item: imageUser, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 0.4, constant: 0)
+        yConstraint.isActive = true
+        infoUserStackView.topAnchor.constraint(equalTo: imageUser.bottomAnchor, constant: 20).isActive = true
+        infoUserStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        infoUserStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        
+        buttonsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        buttonsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        buttonsStackView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20).isActive = true
+    }
+    
+    func addActions() {
+        
+        signInSignUp.addTarget(self, action: #selector(didTapsignInSignUp(_:)), for: .touchUpInside)
+        signOutButton.addTarget(self, action: #selector(didTapSignOut(_:)), for: .touchUpInside)
+        deleteAccountButton.addTarget(self, action: #selector(didTapDeleteAccount(_:)), for: .touchUpInside)
+        
+//        signOutButton.configurationUpdateHandler = { [weak self] button in
+//
+//            guard let signingIn = self?.signingIn else {return}
+//            var config = button.configuration
+//            config?.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+//                var outgoing = incoming
+//                outgoing.foregroundColor = .white
+//                outgoing.font = UIFont.boldSystemFont(ofSize: 15)
+//                return outgoing
+//            }
+//            config?.imagePadding = 10
+//            config?.imagePlacement = .trailing
+//            config?.showsActivityIndicator = signingIn
+//            config?.title = signingIn ? "Signing In..." : "Sign In"
+//            button.isUserInteractionEnabled = !signingIn
+//            button.configuration = config
+//        }
+    }
+    
+    
+    @objc func didTapsignInSignUp(_ sender: UIButton) {
+        print("didTapsignInSignUp")
+    }
+    
+    @objc func didTapSignOut(_ sender: UIButton) {
+        print("didTapSignOut")
+    }
+    
+    @objc func didTapDeleteAccount(_ sender: UIButton) {
+        print("didTapSignOut")
     }
     
 }
